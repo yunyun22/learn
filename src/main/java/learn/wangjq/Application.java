c9cfd5efb4a9e6f9955e2b8d8e61918ecbda559f
yunyun22 <470423980@qq.com>
2017-12-29 08:19:53 +0800

    update index

--- src/main/java/learn/wangjq/Application.java
+++ src/main/java/learn/wangjq/Application.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -19,12 +20,17 @@ public class Application {
         return "Hello World!";
     }
 
-    @RequestMapping("/root")
-    public ModelAndView index() {
-        System.out.println("start in index");
-        return new ModelAndView("index.html");
+    @RequestMapping("/world")
+    @ResponseBody
+    public String world(String json) {
+        return "Hello World!";
     }
 
+    @RequestMapping("/root")
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
