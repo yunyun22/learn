52965e1420b4f7d02513fbb04274fe79aec707e4
yunyun22 <470423980@qq.com>
2018-03-03 12:32:19 +0800

    test BeanFactory

--- src/main/java/learn/wangjq/BeanDefinition/Tester.java
+++ src/main/java/learn/wangjq/BeanDefinition/Tester.java
@@ -2,16 +2,20 @@ package learn.wangjq.BeanDefinition;
 
 import org.springframework.beans.factory.annotation.AnnotatedGenericBeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.support.SimpleBeanDefinitionRegistry;
 import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
+import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
 @Component("t")
 public class Tester {
+
+
     public static void main(String[] args) throws Exception {
         AnnotatedGenericBeanDefinition beanDefinition = new AnnotatedGenericBeanDefinition(Tester.class);
         System.out.println(beanDefinition.getMetadata().getAnnotationTypes());
@@ -37,7 +41,28 @@ public class Tester {
 
         AnnotatedBeanDefinitionReader reader = new AnnotatedBeanDefinitionReader(registry);
         reader.register(Tester.class);
-        System.out.println( registry.getBeanDefinitionCount());
+        System.out.println(registry.getBeanDefinitionCount());
 
+
+        DefaultListableBeanFactory defaultListableBeanFactory = new DefaultListableBeanFactory();
+
+        ClassPathBeanDefinitionScanner classPathBeanDefinitionScanner = new ClassPathBeanDefinitionScanner(defaultListableBeanFactory);
+
+        int count = classPathBeanDefinitionScanner.scan("learn.wangjq.BeanDefinition");
+
+        System.out.println("count = " + count);
+
+        Tester t = (Tester) defaultListableBeanFactory.getBean("t");
+
+        System.out.println("t = " + t);
+
+        String[] beanDefinitionNames = defaultListableBeanFactory.getBeanDefinitionNames();
+
+        for (String s :
+                beanDefinitionNames) {
+
+            System.out.println("s = " + s);
+
+        }
     }
 }
