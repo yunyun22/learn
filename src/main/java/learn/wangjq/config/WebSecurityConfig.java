883da158d145cb54cca066f61a28c7b8c74b81d8
yunyun22 <470423980@qq.com>
2018-03-06 21:42:27 +0800

    test spring security

--- src/main/java/learn/wangjq/config/WebSecurityConfig.java
+++ src/main/java/learn/wangjq/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
 @Configuration
 @EnableWebSecurity
@@ -25,8 +26,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
-
                 .permitAll();
+
+        http.csrf().disable();
     }
 
     @Autowired
