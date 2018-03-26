1e2c4425938af155ad3267903586bd2950c6b5df
yunyun22 <470423980@qq.com>
2018-03-26 17:07:57 +0800

    learn algorithm

---
+++ src/main/java/learn/wangjq/algorithm/MySort.java
@@ -0,0 +1,17 @@
+package learn.wangjq.algorithm;
+
+public class MySort {
+
+    public static int[] bubbleSort(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (numbers[j] < numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+        return numbers;
+    }
+}
