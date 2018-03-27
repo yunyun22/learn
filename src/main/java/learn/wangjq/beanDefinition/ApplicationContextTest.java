31eb60380f57954a1257367ffbf264a5ff972313
yunyun22 <470423980@qq.com>
2018-03-27 15:45:03 +0800

    applicationContext

--- src/main/java/learn/wangjq/beanDefinition/ApplicationContextTest.java
+++ src/main/java/learn/wangjq/beanDefinition/ApplicationContextTest.java
@@ -5,9 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationContextTest {
 
     public static void main(String[] args) {
-
-        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext();
-
+        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("/config/ApplicationContextTest.xml");
+        Student s = (Student) classPathXmlApplicationContext.getBean("student");
+        System.out.println(s);
     }
 
 }
