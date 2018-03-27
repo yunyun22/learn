bc61c7c68673b8de32e89529a98c46a0d18e6880
yunyun22 <470423980@qq.com>
2018-03-27 15:28:48 +0800

    applicationContext

---
+++ src/main/java/learn/wangjq/beanDefinition/ApplicationContextTest.java
@@ -0,0 +1,13 @@
+package learn.wangjq.beanDefinition;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ApplicationContextTest {
+
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext();
+
+    }
+
+}
