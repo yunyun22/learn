3a885b6251c1f2a307bafb984d3fef69f7041da6
yunyun22 <470423980@qq.com>
2018-04-12 16:10:18 +0800

    test  juc

---
+++ src/main/java/learn/wangjq/thread/juc/CyclicBarrierTest.java
@@ -0,0 +1,33 @@
+package learn.wangjq.thread.juc;
+
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
+
+public class CyclicBarrierTest {
+
+    public static void main(String[] args) {
+
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(5);
+
+        Runnable r = () -> {
+
+            try {
+                System.out.println("this thread start wait,having waited threads:"+cyclicBarrier.getNumberWaiting());
+                cyclicBarrier.await();
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+            System.out.println("this thread end");
+        };
+        Thread[] ts = new Thread[5];
+        for (Thread t : ts) {
+            t = new Thread(r);
+            t.start();
+        }
+    }
+}
---
+++ src/main/java/learn/wangjq/thread/juc/SemaphoreTest.java
@@ -0,0 +1,27 @@
+package learn.wangjq.thread.juc;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class SemaphoreTest {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(5);
+        Runnable r = () -> {
+            try {
+                semaphore.acquire();
+                System.out.println("this thread waited");
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }finally {
+                semaphore.release();
+            }
+
+        };
+        Thread[] ts = new Thread[50];
+        for (Thread t : ts) {
+            t = new Thread(r);
+            t.start();
+        }
+    }
+}
