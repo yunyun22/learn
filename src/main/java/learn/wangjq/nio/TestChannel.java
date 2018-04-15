85dec8c9cabb4c5ffb2c662084a2d159f9aa626b
yunyun22 <470423980@qq.com>
2018-04-15 15:07:15 +0800

    test channel

--- src/main/java/learn/wangjq/nio/TestChannel.java
+++ src/main/java/learn/wangjq/nio/TestChannel.java
@@ -1,14 +1,46 @@
 package learn.wangjq.nio;
 
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
 
 public class TestChannel {
 
 
-    public static void main(String[] args) {
-        dispersedReadAndGatherWrite("test.sql");
+    public static void main(String[] args) throws IOException {
+
+//        dispersedReadAndGatherWrite("test.sql");
+        ReadableByteChannel readableByteChannel = Channels.newChannel(System.in);
+        System.out.println("input end");
+        WritableByteChannel writableByteChannel = Channels.newChannel(System.out);
+        System.out.println("output end");
+        copyChannel(readableByteChannel, writableByteChannel);
+        System.out.println("copy end");
+        readableByteChannel.close();
+        writableByteChannel.close();
+    }
+
+
+    public static void copyChannel(ReadableByteChannel readableByteChannel, WritableByteChannel writableByteChannel) throws IOException {
+
+        ByteBuffer buffer = ByteBuffer.allocate(16 * 1024);
+
+        while (readableByteChannel.read(buffer) != -1) {
+            buffer.flip();
+            writableByteChannel.write(buffer);
+            buffer.compact();
+        }
+
+        buffer.flip();
+
+        while (buffer.hasRemaining()) {
+            writableByteChannel.write(buffer);
+        }
+
     }
 
 
