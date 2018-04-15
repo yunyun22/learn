5d5fd4a35990b37d4c9408bb3a56f9032780db25
yunyun22 <470423980@qq.com>
2018-04-15 13:40:15 +0800

    test channel

---
+++ src/main/java/learn/wangjq/nio/TestBuffer.java
@@ -0,0 +1,63 @@
+package learn.wangjq.nio;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.CharBuffer;
+
+public class TestBuffer {
+
+    public void testBufferView () {
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(7).order(ByteOrder.BIG_ENDIAN);
+        CharBuffer charBuffer = byteBuffer.asCharBuffer();
+
+        byteBuffer.put(0, (byte) 0);
+        byteBuffer.put(1, (byte) 'H');
+        byteBuffer.put(2, (byte) 0);
+        byteBuffer.put(3, (byte) 'i');
+        byteBuffer.put(4, (byte) 0);
+        byteBuffer.put(5, (byte) '!');
+        byteBuffer.put(6, (byte) 0);
+
+        System.out.println(byteBuffer);
+        System.out.println(charBuffer);
+
+    }
+
+    public static void main(String[] args) {
+
+        CharBuffer buffer = CharBuffer.allocate(100);
+
+        String s = "hello world";
+
+        if (fillBuffer(s, buffer)) {
+            buffer.flip();
+//            char[] chars = new char[buffer.limit()];
+//            buffer.get(chars);
+//            System.out.println(new String(chars));
+            drainBuffer(buffer);
+        }
+
+    }
+
+    private static boolean drainBuffer(CharBuffer buffer) {
+        while (buffer.hasRemaining()) {
+            char c = buffer.get();
+            System.out.print(c);
+        }
+        return true;
+    }
+
+    private static boolean fillBuffer(String strings, CharBuffer buffer) {
+        if (strings.length() > buffer.remaining()) {
+            return false;
+        }
+
+        for (char c :
+                strings.toCharArray()) {
+            buffer.put(c);
+        }
+        return true;
+    }
+
+}
---
+++ src/test/java/learn/wangjq/nio/TestBufferTest.java
@@ -0,0 +1,61 @@
+package learn.wangjq.nio;
+
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.CharBuffer;
+
+/**
+ * TestBuffer Tester.
+ *
+ * @author wangjq
+ * @version 1.0
+ * @since <pre>04/03/2018</pre>
+ */
+public class TestBufferTest {
+
+
+    @Test
+    public void testTestBufferView() throws Exception {
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(7).order(ByteOrder.BIG_ENDIAN);
+        CharBuffer charBuffer = byteBuffer.asCharBuffer();
+
+        byteBuffer.put(0, (byte) 0);
+        byteBuffer.put(1, (byte) 'H');
+        byteBuffer.put(2, (byte) 0);
+        byteBuffer.put(3, (byte) 'i');
+        byteBuffer.put(4, (byte) 0);
+        byteBuffer.put(5, (byte) '!');
+        byteBuffer.put(6, (byte) 0);
+
+        System.out.println(byteBuffer.toString());
+        System.out.println(charBuffer);
+    }
+
+    @Test
+    public void testByteBufferView() {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(7).order(ByteOrder.BIG_ENDIAN);
+
+        byteBuffer.putInt(-0257);
+
+        byteBuffer.flip();
+
+//        int i = byteBuffer.getInt();
+//
+//        System.out.println("i = " + i);
+
+
+        while (byteBuffer.hasRemaining()) {
+
+            byte b = byteBuffer.get();
+
+            System.out.print(b);
+        }
+
+
+    }
+
+
+} 
