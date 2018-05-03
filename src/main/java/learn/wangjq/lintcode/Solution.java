ff58f2d810029e1be2d4aff928904ebb907f4b4d
yunyun22 <470423980@qq.com>
2018-05-03 14:03:59 +0800

    add lintcode

---
+++ src/main/java/learn/wangjq/lintcode/Solution.java
@@ -0,0 +1,25 @@
+package learn.wangjq.lintcode;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int result = reverseInteger(123);
+        System.out.println("result = " + result);
+    }
+    /**
+     * @param number: A 3-digit number.
+     * @return: Reversed number.
+     */
+    public static int reverseInteger(int number) {
+
+        int i = number / 100;
+        int j = (number - i * 100) / 10;
+        int k = (number - i * 100) - j * 10;
+
+        return k * 100 + j * 10 + i;
+    }
+
+
+
+}
