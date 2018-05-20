880000f7a35ad33fcbbd55981c67876814a2edc1
yunyun22 <470423980@qq.com>
2018-05-20 22:34:04 +0800

    test restTemplate

--- demo/src/main/java/learn/wangjq/Application.java
+++ demo/src/main/java/learn/wangjq/Application.java
@@ -1,18 +1,66 @@
 package learn.wangjq;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @EnableDiscoveryClient
+@Controller
 public class Application {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient; //just here for testing
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String getHelloFromRrgister(){
+
+        isInstanceAvailable();
+        return restTemplate.getForObject("http://demo/helloWorld",String.class);
+    }
+
+    @RequestMapping("/helloWorld")
+    @ResponseBody
+    public String helloWorld(){
+        return "hello world";
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+    }
+
+    private Boolean isInstanceAvailable(){
+        List<ServiceInstance> instances = discoveryClient.getInstances("demo");
+        System.out.println("get instance:"+instances);
+        for(ServiceInstance si : instances){
+            System.out.print("service:");
+            System.out.print(si.getUri().toString());
+        }
+        return instances.size() > 0;
+    }
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 //    @Autowired
