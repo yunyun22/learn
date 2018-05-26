1d0ccf1f3162b5eff8fce92ac0337c2665934f35
yunyun22 <470423980@qq.com>
2018-05-26 19:55:21 +0800

    Merge branch 'master' of github.com:yunyun22/learn
    # Conflicts:
    #	demo/src/main/java/demo/wangjq/algorithm/tree/ReBulidBinaryTree.java

--- demo/src/main/java/demo/wangjq/algorithm/tree/ReBulidBinaryTree.java
+++ demo/src/main/java/demo/wangjq/algorithm/tree/ReBulidBinaryTree.java
@@ -1,26 +1,59 @@
-package learn.wangjq.algorithm.tree;
+package demo.wangjq.algorithm.tree;
 
-import com.sun.javafx.sg.prism.NodeEffectInput;
-import learn.wangjq.ArraysUtil;
+import demo.wangjq.ArraysUtil;
 
 import java.util.Arrays;
 
-import static learn.wangjq.ArraysUtil.getLength;
 
 public class ReBulidBinaryTree {
-
 
     public static void main(String[] args) {
         int[] preorder = {1, 2, 4, 7, 3, 5, 6, 8};
         int[] midorder = {4, 7, 2, 1, 5, 3, 8, 6};
-        reBulidBinaryTree(preorder, midorder);
+        Node node = reBulidBinaryTree(preorder, midorder);
+
+        preorder(node);
+        System.out.println();
+        midorder(node);
+
     }
 
     public static Node reBulidBinaryTree(int[] preorder, int[] midorder) {
         return Node.reBuild(preorder, midorder);
     }
 
+    /**
+     * 前序遍历
+     *
+     * @param node
+     */
+    public static void preorder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.value + " ");
+        preorder(node.left);
+        preorder(node.right);
+    }
 
+    /**
+     * 中序遍历
+     *
+     * @param node
+     */
+    public static void midorder(Node node) {
+        if (node == null) {
+            return;
+        }
+        midorder(node.left);
+        System.out.print(node.value + " ");
+        midorder(node.right);
+    }
+
+
+    /**
+     * 定义二叉树的数据结构
+     */
     private static class Node {
         private Node left;
         private Node right;
@@ -35,46 +68,39 @@ public class ReBulidBinaryTree {
             this.value = value;
         }
 
+        /**
+         * 重构二叉树
+         *
+         * @param preorder
+         * @param midorder
+         * @return
+         */
         public static Node reBuild(int[] preorder, int[] midorder) {
-
+            if (preorder.length <= 0 || midorder.length <= 0) {
+                return null;
+            }
 
             int root = preorder[0];
+            /**
+             * root 节点在中序中的位置
+             */
             int indexOfRoot = ArraysUtil.indexof(midorder, root);
-
             /**
              * 构建左子树
              */
-            Node left = null;
+            int rightLength = indexOfRoot;
+            int[] leftPreorder = Arrays.copyOfRange(preorder, 1, rightLength + 1);
+            int[] leftMidorder = Arrays.copyOfRange(midorder, 0, rightLength);
 
-            if (getLength(midorder, root) > 0 && midorder.length + 1 > 1) {
-
-                int rightLength = indexOfRoot;
-
-                left = reBuild(
-                        Arrays.copyOfRange(preorder, 1, rightLength + 1),
-                        Arrays.copyOfRange(midorder, 0, rightLength));
-            } else {
-                System.out.println("left preorder:" + ArraysUtil.arraysToString(preorder));
-                System.out.println("left midorder:" + ArraysUtil.arraysToString(midorder));
-                return new Node();
-            }
-
+            Node left = reBuild(leftPreorder, leftMidorder);
             /**
              * 构建右子树
              */
-            Node right = null;
-            if (midorder.length > getLength(midorder, root) && preorder.length > midorder.length + 1) {
+            int leftLength = indexOfRoot + 1;
+            int[] rightPreorder = Arrays.copyOfRange(preorder, leftLength, preorder.length);
+            int[] rightMidorder = Arrays.copyOfRange(midorder, leftLength, midorder.length);
 
-                int leftLength = indexOfRoot + 1;
-
-                right = reBuild(
-                        Arrays.copyOfRange(preorder, leftLength, preorder.length),
-                        Arrays.copyOfRange(midorder, leftLength, midorder.length));
-            } else {
-                System.out.println("right preorder:" + ArraysUtil.arraysToString(preorder));
-                System.out.println("right midorder:" + ArraysUtil.arraysToString(midorder));
-                return new Node();
-            }
+            Node right = reBuild(rightPreorder, rightMidorder);
 
             return new Node(left, right, root);
         }
