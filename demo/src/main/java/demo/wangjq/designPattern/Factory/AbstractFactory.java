17f4adea21acddf2a3147c1a0c5d3c5f32c3b48e
wangjinqiao <wangjinqiao@gutou.com>
2018-07-04 17:57:38 +0800

    factory test

---
+++ demo/src/main/java/demo/wangjq/designPattern/Factory/AbstractFactory.java
@@ -0,0 +1,14 @@
+package demo.wangjq.designPattern.Factory;
+
+/**
+ * Created by wangjq on 2018/7/4.
+ */
+public class AbstractFactory {
+}
+
+
+class Car {
+}
+
+class tyre {
+}
---
+++ demo/src/main/java/demo/wangjq/designPattern/Factory/Factory.java
@@ -0,0 +1,32 @@
+package demo.wangjq.designPattern.Factory;
+
+/**
+ * Created by wangjq on 2018/7/4.
+ */
+public abstract class Factory {
+
+    public abstract Fruit creatFruit();
+
+}
+
+class AppleFactory extends Factory {
+
+    @Override
+    public Fruit creatFruit() {
+        return new Apple();
+    }
+}
+
+class PearFactory extends Factory {
+
+    @Override
+    public Fruit creatFruit() {
+        return new Pear();
+    }
+}
+
+
+
+
+
+
