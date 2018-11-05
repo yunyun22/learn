efc66ea247beb97076992be13922715224ce026f
wangjinqiao <wangjinqiao@gutou.com>
2018-11-05 18:05:46 +0800

    计算int的位数

--- demo/src/main/java/demo/wangjq/test/DSTest.java
+++ demo/src/main/java/demo/wangjq/test/DSTest.java
@@ -3,7 +3,8 @@ package demo.wangjq.test;
 
 public class DSTest {
     public static void main(String[] args) throws InterruptedException {
-        incre();
+        //System.out.println(Integer.bitCount(100));
+        System.out.println(bitCount(18787846));
     }
 
     public static void incre() throws InterruptedException {
@@ -14,5 +15,20 @@ public class DSTest {
             i++;
         }
     }
+
+    public static int bitCount(int n) {
+        int i = 10;
+        int bits = 1;
+        while (true) {
+            //Math.abs(n / i)意思是取n/i的绝对值
+            if (Math.abs(n / i) > 0) {
+                i *= 10;
+                bits++;
+            } else {
+                return bits;
+            }
+        }
+    }
+
 
 }
