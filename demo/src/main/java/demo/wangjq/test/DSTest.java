af5248635b252f64eb92d1052924bebfa564845a
yunyun22 <470423980@qq.com>
2018-11-16 19:19:51 +0800

    add repackage

--- demo/src/main/java/demo/wangjq/test/DSTest.java
+++ demo/src/main/java/demo/wangjq/test/DSTest.java
@@ -4,7 +4,9 @@ package demo.wangjq.test;
 public class DSTest {
     public static void main(String[] args) throws InterruptedException {
         //System.out.println(Integer.bitCount(100));
-        System.out.println(bitCount(18787846));
+        //System.out.println(bitCount(18787846));
+
+        System.out.println(Integer.reverseBytes(123));
     }
 
     public static void incre() throws InterruptedException {
