a06c7040ffd8381ecb36e749ce36662205f2ab85
wangjinqiao <wangjinqiao@gutou.com>
2019-01-25 09:26:25 +0800

    test Integer.bitCount

--- demo/src/main/java/demo/wangjq/lintcode/Solution.java
+++ demo/src/main/java/demo/wangjq/lintcode/Solution.java
@@ -149,4 +149,24 @@ public class Solution {
     public void testReverse() {
         System.out.println(reverse(1534236469));
     }
+
+    public static int maxArea(int[] height) {
+        int left = 0, right = height.length - 1, max = 0;
+        while (left < right) {
+            max = Math.max(height[left] > height[right] ? (height[right] * (right - left)) : (height[left] * (right - left)), max);
+            if (height[left] > height[right]) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return max;
+    }
+
+    @Test
+    public void testMaxArea() {
+        int[] height = new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7};
+        Assert.assertEquals(49, maxArea(height));
+        Assert.assertEquals(1, maxArea(new int[]{1, 1}));
+    }
 }
