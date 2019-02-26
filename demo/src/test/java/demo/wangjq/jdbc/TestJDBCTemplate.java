98187c23562e0977bffe17311df81e19b47fa7d5
wangjinqiao <wangjinqiao@gutou.com>
2019-02-26 17:11:19 +0800

    添加jdbc

--- demo/src/test/java/demo/wangjq/jdbc/TestJDBCTemplate.java
+++ demo/src/test/java/demo/wangjq/jdbc/TestJDBCTemplate.java
@@ -2,9 +2,15 @@ package demo.wangjq.jdbc;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.util.List;
+import java.util.Map;
 
 import demo.wangjq.BaseTest;
+import demo.wangjq.app.domain.UmsMember;
 
 /**
  * Created by wangjq on 2019/2/26.
@@ -15,10 +21,14 @@ public class TestJDBCTemplate extends BaseTest {
     private JdbcTemplate jdbcTemplate;
 
 
-    private static final String sql = "select * from ums_member";
+    private static final String sql = "SELECT * FROM ums_member";
 
     @Test
     public void testQuery() {
-        jdbcTemplate.queryForList(sql);
+        RowMapper<UmsMember> rm = BeanPropertyRowMapper.newInstance(UmsMember.class);
+        List<UmsMember> list = jdbcTemplate.query(sql, rm);
+        for (UmsMember umsMember : list) {
+            System.out.println(umsMember);
+        }
     }
 }
