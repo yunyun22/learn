14d7f42ff21b5d29ad2570db79d809dd54593934
wangjinqiao <wangjinqiao@gutou.com>
2019-04-04 18:15:13 +0800

    33. Search in Rotated Sorted Array

--- demo/src/main/java/demo/wangjq/algorithm/lintcode/ThirtyToForty.java
+++ demo/src/main/java/demo/wangjq/algorithm/lintcode/ThirtyToForty.java
@@ -1,5 +1,7 @@
 package demo.wangjq.algorithm.lintcode;
 
+import org.junit.Test;
+
 import java.util.Stack;
 
 /**
@@ -15,5 +17,31 @@ public class ThirtyToForty {
         Stack<Character> stack = new Stack<>();
         return 0;
 
+    }
+
+
+    public static int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, mid;
+        while (left <= right) {
+            mid = (right + left) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            }
+            if (target >= nums[left] && target <= nums[mid]) {
+                right = mid;
+            } else if (target >= nums[mid] && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return -1;
+    }
+
+    @Test
+    public void testSearch() {
+        int[] nums = {5, 1, 3};
+        int target = 5;
+        search(nums, target);
     }
 }
