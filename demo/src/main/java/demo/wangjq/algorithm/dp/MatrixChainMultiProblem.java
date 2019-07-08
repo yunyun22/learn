15b6a3875018563704a1ae76986b4737adef25bf
wangjinqiao <wangjinqiao@gutou.com>
2019-07-08 17:22:27 +0800

    matrixChain

--- demo/src/main/java/demo/wangjq/algorithm/dp/MatrixChainMultiProblem.java
+++ demo/src/main/java/demo/wangjq/algorithm/dp/MatrixChainMultiProblem.java
@@ -8,8 +8,44 @@ public class MatrixChainMultiProblem {
 
     public static void main(String[] args) {
 
-        System.out.println(matrixChainCount(4));
+        //List<String> list = new ArrayList<>();
 
+
+        test();
+
+    }
+
+
+    /**
+     * 矩阵计算次数的最优解
+     *
+     * @param matrix 矩阵 其中index==length-1时，是最后矩阵的列
+     * @return 最优解
+     */
+    public static int minMatrixChainCount(int[] matrix) {
+
+        int n = matrix.length - 1;
+
+        /**
+         * 矩阵的个数
+         */
+        int l = n - 1;
+
+        int[][] opt = new int[n - 1][n - 1];
+
+        for (int i = 0; i < n - 1; i++) {
+            opt[i][i] = 0;
+        }
+
+        for (int d = 1; d < l; d++) {
+            for (int i = 1; i < l - d; i++) {
+                int j = i + d;
+                for (int k = i + 1; k < j; l++) {
+                    opt[i][j] = Math.min(opt[i][j], opt[i][k - 1] + opt[k][j] + matrix[i] * matrix[k] * matrix[j + 1]);
+                }
+            }
+        }
+        return opt[1][n - 1];
     }
 
 
@@ -28,5 +64,20 @@ public class MatrixChainMultiProblem {
 
     }
 
+
+    public static void test() {
+
+        for (int d = 0; d < 6; d++) {
+            for (int i = 0; i < 6 - d; i++) {
+                int j = i + d;
+                System.out.print("i=" + i + ",j=" + j);
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+
+    }
 
 }
