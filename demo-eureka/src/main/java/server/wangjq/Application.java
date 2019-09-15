fee0ae2d3fac9ee5f7fcd7d3d31cd89a656519ae
yunyun22 <470423980@qq.com>
2019-09-15 21:52:48 +0800

    test eureka

---
+++ demo-eureka/src/main/java/server/wangjq/Application.java
@@ -0,0 +1,16 @@
+package server.wangjq;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(WebApplicationType.SERVLET).run(args);
+    }
+
+}
